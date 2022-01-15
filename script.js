952398b09be75e78be615d2fbd3f148a53163e22
Gourav <68805979+GouravChanalia@users.noreply.github.com>
2022-01-15 20:04:12 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,22 +1,25 @@
+const toasterContainer = document.getElementById("toaster-container");
 const domain = document.getElementById("domain")
 const url = document.getElementById("url")
 const addBtn = document.getElementById("add")
 const table = document.getElementById( "myTable" )
 const openAllBtn = document.getElementById( "openAll" )
 
-let cellDomain, cellURL, cellAction, btnDelete, btnOpenUrl
+let cellDomain, cellURL, cellAction, btnDelete, btnCopyURL, btnOpenUrl, urlDiv, urlCopyBtn ;
 
 const isOccupied = () => domain.value != '' && url.value != ''
 
 const addRow = ( domain, url, flag = 0) => {
 
-    //Creating and adding row and cells in the table
+    /* Creating and adding row and cells in the table */
     const row = table.insertRow( table.rows.length )
 
+    /* Create button Delete */
     btnDelete = document.createElement( 'button' )
     btnDelete.innerHTML = 'Delete'
     btnDelete.className = 'btnDelete'
 
+    /* Create button Open */
     btnOpenUrl = document.createElement( 'button' )
     btnOpenUrl.innerHTML = 'Open'
     btnOpenUrl.className = 'btnOpen'
@@ -27,11 +30,36 @@ const addRow = ( domain, url, flag = 0) => {
 
     cellDomain.innerHTML = domain;
     cellDomain.style.fontSize = "1.1em"
-    cellURL.innerHTML = url;
+    
+    /* preparing div and child elements to be appended inside cellUrl */
+    urlDiv = document.createElement( 'div' );
+    urlDiv.innerHTML = url;
+    urlCopyBtn = document.createElement( 'span' );
+    urlCopyBtn.className = "material-icons btnCopyUrl";
+    urlCopyBtn.innerHTML = "&nbsp;&nbsp;content_paste";
+    urlDiv.appendChild( urlCopyBtn );
 
-    cellAction.appendChild( btnOpenUrl )
-    if( !flag )
-    cellAction.appendChild( btnDelete )
+    /* append buttons to table row */
+    cellAction.appendChild( btnOpenUrl );
+    cellURL.appendChild( urlDiv );
+    !flag ? cellAction.appendChild( btnDelete ) : "";
+
+    /* event Listeners */
+    urlCopyBtn.addEventListener( 'click', () => {
+
+        /* copy url to clipboard */
+        navigator.clipboard.writeText( url );
+        
+        /* show toaster */
+        const toaster = document.createElement( 'div' );
+        toaster.className = "toaster";
+        toaster.innerHTML = "Copied!";
+        toasterContainer.appendChild(toaster);
+        setTimeout( () => {
+            toaster.remove();
+        }, 3000 )
+
+    } )
 
     btnDelete.addEventListener( 'click', () => { 
 
@@ -43,7 +71,9 @@ const addRow = ( domain, url, flag = 0) => {
 
     } )
 
-    btnOpenUrl.addEventListener( 'click', () => open( row.cells[1].innerHTML ) )
+    btnOpenUrl.addEventListener( 'click', () => {
+        open( row.cells[1].innerHTML );
+    } )
 
 }
 
@@ -85,9 +115,7 @@ if( !localStorage.getItem( 'table' ) ){
 
 let list = JSON.parse( localStorage.getItem( 'table' ) )
 
-console.log( list )
-
-list.forEach( ({domain, url}, index) => new RegExp('URL Manager','i').test(domain) && url === 'https://github.com/GouravChanalia/URLManager' ?addRow( domain,url,1 ):addRow( domain,url))
+list.forEach( ({domain, url}) => new RegExp('URL Manager','i').test(domain) && url === 'https://github.com/GouravChanalia/URLManager' ?addRow( domain,url,1 ):addRow( domain,url))
 
 addBtn.addEventListener( 'click', callback )
 
